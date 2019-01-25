49ae40791d0b9eb3881ee5891b899df187ff9ee8
A0080 <Aron Strandberg>
2019-01-25 13:46:32 +0100

    api functinality added to client

--- app/public/javascripts/map/stations.js
+++ app/public/javascripts/map/stations.js
@@ -1,46 +1,14 @@
-var stations = [
-    {
-        id: 1,
-        name: "Stockholm",
-        lat: 59.38917842312835,
-        long: 18.193359375,
-        airTemp: -10+"&#8451;",
-        roadTemp: -7+"&#8451;",
-        humidity: 20+"%",
-        windSpeed: 2.5 + " m/s",
-        windDirection: "North"
-    },
-    {
-        id: 2,
-        name: "Malmö",
-        lat: 55.600074841258156,
-        long: 13.02978515625,
-        airTemp: -5+"&#8451;",
-        roadTemp: -3+"&#8451;",
-        humidity: 35+"%",
-        windSpeed: 1.5 + " m/s",
-        windDirection: "South"
-    },
-    {
-        id: 3,
-        name: "Östersund",
-        lat: 63.176517528924876,
-        long: 14.636578559875488,
-        airTemp: -17+"&#8451;",
-        roadTemp: -15+"&#8451;",
-        humidity: 15+"%",
-        windSpeed: 6.5 + " m/s",
-        windDirection: "West"
-    },
-    {
-        id: 4,
-        name: "Luleå",
-        lat: 65.6181962181437,
-        long: 22.051963806152344,
-        airTemp: -20+"&#8451;",
-        roadTemp: -17+"&#8451;",
-        humidity: 21+"%",
-        windSpeed: 5.5 + " m/s",
-        windDirection: "North"
-    },
-]
+
+
+
+function getStations() {
+    $.getJSON("/api/getStationData", function(stations) {
+
+        console.log(stations[0].lat);
+
+
+    });    
+
+
+}
+
