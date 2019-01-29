5e1f848c2d7f2d66c0e1fc2fb3ee821ee998d179
wilant <>
2019-01-29 11:20:43 +0100

    grafer funkar med flera linjer WOOOP

--- app/public/javascripts/graph/getWeatherData.js
+++ app/public/javascripts/graph/getWeatherData.js
@@ -1,12 +1,12 @@
 
 
- function getWeatherData(station_id, start_time, stop_time) {
+async function getWeatherData(station_id, start_time, stop_time) {
 
      //station_id = 'SE_STA_VVIS2240';
      //start_time = '2019-01-20 13:12:12';
      //stop_time = '2019-01-23 13:23:20';
 		
-	 $.getJSON("/api/getWeatherData", {station_id, start_time, stop_time}, function(weatherData) {
+	await $.getJSON("/api/getWeatherData", {station_id, start_time, stop_time}, function(weatherData) {
 
 	
 	//console.log(weatherData);
