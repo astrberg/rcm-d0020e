ecbb1cb02924e81b288d5ab13cad43140280205f
97gushan <97gustavh@gmail.com>
2019-01-31 10:49:39 +0100

    speed up the gathering of avg temps over every county

--- app/database/getProvinceData.js
+++ app/database/getProvinceData.js
@@ -35,20 +35,51 @@ module.exports = {
         mysqlssh.connect(auth.ssh, auth.database).then(client => {
             
             // select avg temps from stations in county over the last 15 min
-            var sql = "SELECT AVG(air_temperature), AVG(road_temperature) FROM weather_data WHERE station_id "+ 
-                        "IN (SELECT id FROM station_data WHERE county_number = ?) " + 
-                        "AND timestamp BETWEEN ? AND ?";
+            var sql = "select * from (\
+                            select * from (\
+                                select w.id, w.air_temperature, s.county_number from weather_data w, station_data s \
+                                    where w.station_id = s.id) as g order by g.id desc limit 576) as t order by t.county_number asc";
             
-                        
+                    
             var values =  [county_number, start_time.toISOString(), stop_time.toISOString()];
 
-            client.query(sql, values, function (err, results) {
+            client.query(sql, function (err, results) {
                 if (err) throw err
                 
                 mysqlssh.close()
+                
+                var temperatures = [];
+                
+                // smalest county number is 2
+                var county = 2;
+
+                // highest county number is 25, loop up to that
+                while(county < 26){
+                    var temporary_temp= 0;
+                    var stations_per_county = 0;
+
+                    // loop through the given values and for every element that lies in a specific county
+                    for(var i = 0; i < results.length; i++){
+                        if(results[i].county_number == county){
+                            // add the temperature of the station in the county and count up 1
+                            temporary_temp += results[i].air_temperature;
+                            stations_per_county++;
+                        }
+                        
+                    }
+                    // if there is any stations in a county, add the calculated avg to the list
+                    if(stations_per_county > 0){
+                        temperatures.push([county,temporary_temp/stations_per_county]);
+
+                    }
+
+                    county++;
+                }
+                
+                
 
                 // send data back to client
-                res.send(results);
+                res.send(temperatures);
             });
 
         }).catch(err => {
