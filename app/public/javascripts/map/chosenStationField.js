bccbe1e47d082d733be7ff8951c2efe1ec1e4502
97gushan <97gustavh@gmail.com>
2019-02-01 13:49:00 +0100

    show and hide button when list is empty or filled

---
+++ app/public/javascripts/map/chosenStationField.js
@@ -0,0 +1,52 @@
+
+
+function displayStationField(){
+
+
+    var div = $("#stationList-container")
+
+    if (div.is(":hidden")) {
+        div.show();
+        addStationsToField();
+    
+    } else {
+        
+        div.hide();
+    }
+}
+
+
+// index is the stations index in the list of chosen stations
+function getStationBox(index, name){
+    var stationDiv = `<div class="stationBox stationBox${index}"> 
+                        <div class="stationBox-container">
+                            <h3> Station name: '${name}'</h3> 
+                            <button class="removeStation" onclick="removeStationViaList('${chosenStations[index]}',${index})" >Remove</button>
+                        </div>
+                      </div>`;
+
+    return stationDiv;
+}
+
+
+
+function addStationsToField(){
+    $("#stationList-container").empty();
+    
+    for(var i = 0; i < chosenStations.length; i++){
+        $("#stationList-container").append(getStationBox(i, chosenStations[i].name));
+    }
+   
+}
+
+function showStationButton(){
+    var button = $("#stationList-button");
+    button.show();
+    
+}
+
+function hideStationButton(){
+    var button = $("#stationList-button");
+    button.hide();
+    
+}
