7381e919c31946b190fdb8abd79beee472e4037d
97gushan <97gustavh@gmail.com>
2019-02-05 15:56:04 +0100

    moved code from map to new fiels

---
+++ app/public/javascripts/map/markers.js
@@ -0,0 +1,99 @@
+
+var icon = L.divIcon({
+    className: 'fa fa-map-marker fa-2x',
+    iconAnchor: [12, 24],
+    popupAnchor: [-5, -25]
+});
+var selectedIcon = L.divIcon({
+    className: 'fa fa-map-marker fa-3x',
+    iconAnchor: [15, 34],
+    popupAnchor: [-5, -35]
+});
+
+
+function addMarkerOnZoom(group){
+    for(var i = 0; i <= group; i++){
+        if(!map.hasLayer(layerGroups[i])){
+            map.addLayer(layerGroups[i]);
+        
+        }
+    }
+}
+
+function removeMarkerOnZoom(group){
+    for(var i = 9; i > group; i--){
+        if(map.hasLayer(layerGroups[i])){
+            map.removeLayer(layerGroups[i]);
+        
+        }
+    }
+    
+}
+
+/**
+ * Adds a station marker with popup content to a map layer
+ */
+var layerGroups = [];
+var timer = Date.now();
+
+function addStationToLayer(station, layerNumber){
+    var marker = L.marker([station.lon, station.lat]);
+    marker.setIcon(icon);
+
+
+    if(!layerGroups[layerNumber]) {
+        layerGroups[layerNumber] = new L.layerGroup();
+    }
+
+    layerGroups[layerNumber].addLayer(marker);    
+    
+    // Add popup content
+    addPopup(station, marker);
+   
+
+    marker.on('click', async function(){
+        changeButtonState(station);
+        
+        // Wait for weather data
+        await getLatestWeatherData(station.id);
+        let dataText = [" \xB0C", " \xB0C", " %", " m/s", ""];
+        let data = ["air_temperature", "road_temperature", "air_humidity", "wind_speed", "wind_direction"];
+        let markerDiv = document.getElementById("marker-data");
+        if(markerDiv != null) {
+            for(let i = 0; i < 4; i++) {
+                markerDiv.rows[i + 2].cells[1].innerHTML = latestWeatherData[0][data[i]] + dataText[i];
+            }
+            markerDiv.rows[6].cells[1].innerHTML =  windDirection(latestWeatherData[0][data[4]]);
+        }   
+        
+
+    });
+
+
+}
+
+function createLayers(stations){
+    // add every tenth station to the first layer
+    for(var i = 0; i< stations.length; i+=10){
+        addStationToLayer(stations[i], 0);
+    }
+
+    // add every fifth station to the jth layer
+    for(var j = 0; j < 5; j++){
+        for(var i = j; i< stations.length; i+=5){
+            // skip every tenth station
+            if(i % 10 != 0){
+
+                // merge the fourth and fifth layers into one
+                if(j == 4){
+                    addStationToLayer(stations[i], j);
+                }else{
+                    addStationToLayer(stations[i], j+1);
+                }
+                
+            }
+        }
+    }
+
+    map.addLayer(layerGroups[0])
+}
---
+++ app/public/javascripts/map/stationPopup.js
@@ -0,0 +1,78 @@
+
+// Popup content
+function addPopup(station, marker) {
+    var div = document.createElement("table-data");
+    div.id = "popupid:" + station.id;
+    div.innerHTML = 
+    '<table id = "marker-data" >' +
+            '<tr> <td> Station </td><td>' + station.name +'</td></tr>' + 
+            '<tr> <td> Län: </td><td>' + countyNames[station.county_number] + '</td></tr>' + 
+            '<tr> <td>Lufttemperatur: </td><td></td></tr>' +
+            '<tr> <td>Vägtemperatur: </td><td></td></tr>' +
+            '<tr> <td>Luftfuktighet: </td><td></td></tr>' +
+            '<tr> <td>Vindhastighet: </td><td></td></tr>' +
+            '<tr> <td>Vindriktning: </td><td></td></tr>' +
+    '</table>';
+
+    // Button
+    var button = document.createElement("button");
+    button.id = "buttonid:" + station.id;
+    button.className = "add-button";
+    button.innerText = "Lägg till";
+    button.addEventListener('click', function(){
+        handleChosenStations(station);
+    });
+    div.appendChild(button);
+    marker.bindPopup(div);
+}
+
+function windDirection(data) {
+    if(data == 'north') {
+      return '&nbsp; <i class="fa fa-long-arrow-down"></i> <br>';
+    }
+    else if(data == 'south') {
+      return '&nbsp; <i class="fa fa-long-arrow-up"></i> <br>';
+    }
+    else if(data == 'east') {
+      return '&nbsp; <i class="fa fa-long-arrow-left"></i> <br>';
+    }
+    else if(data == 'west') {
+      return '&nbsp; <i class="fa fa-long-arrow-right"></i> <br>';
+    }
+    else if(data == 'northEast') { //southWest
+      return '&nbsp; <i class="fa fa-long-arrow-down" style="transform: rotate(45deg)"></i> <br>';
+    }
+    else if(data == 'northWest') { //southEast
+      return '&nbsp; <i class="fa fa-long-arrow-right" style="transform: rotate(45deg)"></i> <br>';
+    }
+    else if(data == 'southEast') { //northWest
+      return '&nbsp; <i class="fa fa-long-arrow-up" style="transform: rotate(45deg)"></i> <br>';
+    }
+    else if(data == 'southWest') { //northEast
+      return '&nbsp; <i class="fa fa-long-arrow-left" style="transform: rotate(45deg)"></i> <br>';
+    }
+}
+
+function changeButtonState(station, newState){
+    var button = document.getElementById("buttonid:" + station.id);
+
+    if(button != null){
+        if(newState == "remove"){
+            changeButtonText(button, "remove-button", "Ta bort");
+        
+        }else if(newState == "add"){
+            changeButtonText(button, "add-button", "Lägg till");
+        
+        }else if(chosenStations.includes(station)){
+            changeButtonText(button, "remove-button", "Ta bort");
+        
+        }else{
+            changeButtonText(button, "add-button", "Lägg till");
+        }
+    }
+}
+
+function changeButtonText(button, classText, buttonText){
+    button.className = classText;
+    button.innerText = buttonText;
+}
