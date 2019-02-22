a9fe2e784ee79b7958da62b91416ca2721202430
97gushan <97gustavh@gmail.com>
2019-02-22 12:50:05 +0100

    refactoring

--- app/database/getWeatherData.js
+++ app/database/getWeatherData.js
@@ -18,6 +18,7 @@ module.exports = {
 
         let auth = new authorization.Authorization();
         let weather_data = [];
+
         // ssh to database server and then connect to db
         mysqlssh.connect(auth.ssh, auth.database).then(client => {
             const sql = "SELECT * FROM weather_data WHERE station_id = ? ORDER BY id DESC LIMIT 1";
@@ -38,7 +39,9 @@ module.exports = {
                     // JSON parses timestamp to UTC+0 and we live in UTC+1
                     current_time.setHours(current_time.getHours() - current_time.getTimezoneOffset() / 60);
                     
-                    
+                    // change windspeed from km/h to m/s and use 2 decimals
+                    results[0].wind_speed /=  3.6;
+                    results[0].wind_speed = results[0].wind_speed.toFixed(2);
                     
                     weather_data.push(results);
                     callback();
@@ -82,58 +85,39 @@ module.exports = {
 
                 client.query(sql, values, function (err, results) {
                     if (err) throw err
-    
-                    // convert fetched timestamp to correct timezone. 
-                    // JSON parses timestamp to UTC+0 and we live in UTC+1
+                    
+                    let filtered_result = [];
+                    
+                    // calculate the time difference between the first and last result
+                    let time_diff = results[results.length - 1].timestamp.getTime() - results[0].timestamp.getTime();
+
+
+                    //change time_diff from ms to h
+                    time_diff = time_diff / (3.6*(10**6));
+                    
+                    let i = 0;
+     
                     results.forEach (result =>{
-    
+                        // convert fetched timestamp to correct timezone. 
+                        // JSON parses timestamp to UTC+0 and we live in UTC+1
                         let current_time = result.timestamp;
     
                         current_time.setHours(current_time.getHours() - current_time.getTimezoneOffset() / 60);
-                    });
-                    let filteredResult = [];
-                    let i = 0;
-
-
-                    // calculate the time difference between the first and last result
-                    let timeDiff = results[results.length - 1].timestamp.getTime() - results[0].timestamp.getTime();
-
-
-                    //change timediff from ms to h
-                    timeDiff = timeDiff / (3.6*(10**6));
-                    
-                    //console.log(timeDiff)
-
-                    results.forEach(result =>{
-
-                        // depeding on the timeDiff, filter the result and add 1/1, 1/2, 1/4, 1/8 or 1/16 of every result
                         
-                        // these limits are up for tweaking
-                        if(timeDiff < 96){ // less then 4 days
-                            filteredResult.push(result);
-                        }else if(timeDiff < 252){   // less then 1.5 week
-                            if(i % 2 == 0){
-                                filteredResult.push(result);
-                            }
-                        }else if(timeDiff < 1008){   // less then 6 weeks
-                            if(i % 4 == 0){
-                                filteredResult.push(result);
-                            }
-                        }else if(timeDiff < 2016){  // less then 12 weeks
-                            if(i % 8 == 0){
-                                filteredResult.push(result);
-                            }
-                        }else{
-                            if(i % 16 == 0){
-                                filteredResult.push(result);
-                            }
+                        // change windspeed from km/h to m/s and use 2 decimals
+                        result.wind_speed /=  3.6;
+                        result.wind_speed = result.wind_speed.toFixed(2);
+
+                        // depeding on the time_diff, filter the result and add 1/1, 1/2, 1/4, 1/8 or 1/16 of every result
+                        if(i % calculateMod(time_diff) == 0){
+                            filtered_result.push(result);
                         }
-                        
+
                         i++;
                     });
-                    
+
                     // add the data of the station to the list 
-                    weather_data.push(filteredResult);
+                    weather_data.push(filtered_result);
 
                     // callback to make the async stuff work
                     callback();
@@ -192,3 +176,19 @@ module.exports = {
         })    
     }
 };
+
+
+function calculateMod(hours){
+    // these limits are up for tweaking
+    if(hours < 96){ // less then 4 days
+        return 1;                        
+    }else if(hours < 252){   // less then 1.5 week
+        return 2;
+    }else if(hours < 1008){   // less then 6 weeks
+        return 4;
+    }else if(hours < 2016){  // less then 12 weeks
+        return 8;
+    }else{
+        return 16;
+    }
+}
