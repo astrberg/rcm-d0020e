630ce962daa0d10231f418dd54b07a046034466b
wilant <>
2019-02-27 11:56:54 +0100

    tidsintervall fixed

--- app/public/javascripts/graph/getWeatherData.js
+++ app/public/javascripts/graph/getWeatherData.js
@@ -3,7 +3,6 @@
 async function getWeatherData(station_id, start_time, stop_time,station_name) {		
 	await $.getJSON("/api/getWeatherData", {station_id, start_time, stop_time}, function(weatherData) {
 		for(let i = 0; i < weatherData.length; i++){
-		console.log(weatherData[i])
 				datamultieplegraf(weatherData[i],station_name[i]);
 				datamultieplegrafair(weatherData[i],station_name[i]);
 				datamultieplegrafhumidity(weatherData[i],station_name[i]);
