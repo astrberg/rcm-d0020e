bd0e06163f4cb2055d4aafeebcb93d8939536121
wilant <>
2019-03-01 13:20:23 +0100

    update

--- app/public/javascripts/graph/getWeatherData.js
+++ app/public/javascripts/graph/getWeatherData.js
@@ -15,29 +15,14 @@ async function getWeatherData(station_id, start_time, stop_time,station_name) {
 //Get current latest weatherdata from the stations sent in
 async function getlatest(station_id,station_name) {
     await $.getJSON("/api/getLatestWeatherData", {station_id}, function(weatherData) {
-// loop through every given station id and get the find the 
-	// weatherdata of that station in the latestWeather list
-	for(let j = 0; j < station_id.length; j++){
-
-		for(let i = 0; i < latestWeatherData.length; i++){
-			// when found, draw the graphs
-			if(station_id[j] == latestWeatherData[i].station_id){
-				databarchartcurrent(latestWeatherData[i],station_name[j]);
-				databarchartroadcurrent(latestWeatherData[i],station_name[j]);
-				databarcharthumcurrent(latestWeatherData[i],station_name[j]);
-				databarchartwindcurrent(latestWeatherData[i],station_name[j]);
-				break;
-			}
-		}
 		
-}		
 
-/*for(let i = 0; i < weatherData.length; i++){
+	for(let i = 0; i < weatherData.length; i++){
 			databarchartcurrent(weatherData[i],station_name[i]);
 			databarchartroadcurrent(weatherData[i],station_name[i]);
 			databarcharthumcurrent(weatherData[i],station_name[i]);
 			databarchartwindcurrent(weatherData[i],station_name[i]);
 		}
-    });	*/
+    });	
 }
 
--- app/public/javascripts/graph/graphs.js
+++ app/public/javascripts/graph/graphs.js
@@ -64,12 +64,9 @@ function generatedataforbar(typeofgraph,datatempvar,stationame){
 }
 //current data air temp
 function databarchartcurrent(weatherdata,station_name){
-	console.log(weatherdata[0].air_temperature);
-        console.log(weatherdata.air_temperature);
 	var typeofgraph = "current_air";
 	var stationame = station_name;
-	//var datatempvar= weatherdata[0].air_temperature;
-        var datatempvar= weatherdata.air_temperature;
+	var datatempvar= weatherdata[0].air_temperature;
 	generatedataforbar(typeofgraph,datatempvar,stationame);
 }
 
@@ -99,8 +96,7 @@ function currenttempgraph(weatherdata){
 function databarchartwindcurrent(weatherdata,station_name){
 	var typeofgraph = "current_windspeed";
 	var stationame = station_name;
-	//var datatempvar= weatherdata[0].wind_speed;
-        var datatempvar= weatherdata.wind_speed;
+	var datatempvar= weatherdata[0].wind_speed;
 	generatedataforbar(typeofgraph,datatempvar,stationame);
 }
 
@@ -132,8 +128,7 @@ function currentwindspeedgraph(weatherdata){
 function databarchartroadcurrent(weatherdata,station_name){
 	var typeofgraph = "current_road";
 	var stationame = station_name;
-	//var datatempvar= weatherdata[0].road_temperature;
-        var datatempvar= weatherdata.road_temperature;
+	var datatempvar= weatherdata[0].road_temperature;
 	generatedataforbar(typeofgraph,datatempvar,stationame);
 }
 
@@ -165,8 +160,7 @@ function currentroadtempgraph(weatherdata){
 function databarcharthumcurrent(weatherdata,station_name){
 	var typeofgraph = "current_hum";
 	var stationame = station_name;
-	//var datatempvar= weatherdata[0].air_humidity;
-	var datatempvar= weatherdata.air_humidity;
+	var datatempvar= weatherdata[0].air_humidity;
 	generatedataforbar(typeofgraph,datatempvar,stationame);
 }
 var chart2 = null;
